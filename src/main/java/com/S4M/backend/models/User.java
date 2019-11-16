eec32fedb1100f963ad489c66339c3e9b725c779
Jelle Geraets <jellegeraets@hotmail.com>
2019-11-16 14:57:52 +0100

    fixed basic model for User

--- src/main/java/com/S4M/backend/models/User.java
+++ src/main/java/com/S4M/backend/models/User.java
@@ -1,38 +1,30 @@
 package com.S4M.backend.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.UUID;
 
+@Entity
+@Getter
+@NoArgsConstructor
 public class User {
-    UUID id;
-    String name;
-    String description;
-    String link;
 
-    public User(@JsonProperty("id") UUID id,
+    @Id @GeneratedValue
+    int id;
+    String name;
+    String password;
+    boolean isAdmin;
+
+    public User(@JsonProperty("id") int id,
                 @JsonProperty("name") String name,
-                @JsonProperty("description") String description,
-                @JsonProperty("link") String link) {
+                @JsonProperty("password") String password) {
         this.id = id;
         this.name = name;
-        this.description = description;
-        this.link = link;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getLink() {
-        return link;
+        this.password = password;
     }
 }
