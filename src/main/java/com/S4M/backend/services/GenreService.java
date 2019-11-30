110626786db524dc38b3b480e29999ff10f77e0f
Jelle Geraets <jellegeraets@hotmail.com>
2019-11-30 14:20:41 +0100

    added basics for Genres

---
+++ src/main/java/com/S4M/backend/services/GenreService.java
@@ -0,0 +1,36 @@
+package com.S4M.backend.services;
+
+import com.S4M.backend.models.Genre;
+import com.S4M.backend.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GenreService {
+
+    @Autowired
+    GenreRepository repository;
+
+    public Genre insertGenre(Genre genre) {
+        return repository.save(genre);
+    }
+
+    public List<Genre> getAllGenres() {
+        return repository.findAll();
+    }
+
+    public Optional<Genre> getGenreById(int id) {
+        return repository.findById(id);
+    }
+
+    public void deleteGenreById(int id) {
+        repository.deleteById(id);
+    }
+
+    public Genre updateGenre(Genre genre) {
+        return repository.save(genre);
+    }
+}
